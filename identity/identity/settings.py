fc960fd4b05880c4b14e5f25703463794ba443a1
Kantanand <kantanand.usk@gmail.com>
2018-01-22 20:15:37 +0530

    latest files updated

--- identity/identity/settings.py
+++ identity/identity/settings.py
@@ -171,8 +171,17 @@ JWT_AUTH = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
 # =====================================================================
 # Login - Logout - Login Exemtp & Auth URLs
 # =====================================================================
